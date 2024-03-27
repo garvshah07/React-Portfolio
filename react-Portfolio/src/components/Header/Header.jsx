203fb9e9d1a679a22ff019c34433ab95b65e9d35
garvshah07 <garv8890@gmail.com>
2024-03-27 22:03:38 +0530

    commit

--- react-Portfolio/src/components/Header/Header.jsx
+++ react-Portfolio/src/components/Header/Header.jsx
@@ -2,9 +2,21 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <>
-      <h1 className="flex  justify-center text-3xl   font-bold">Hello world!</h1>
-    </>
+    <header>
+      <nav className="flex justify-between h-16 p-4 bg-white m-3 ">
+        <div className=" flex justify-center w-[50%]">
+          <i>Logo</i>
+        </div>
+        <div className=" w-[50%]">
+          <ul className="flex gap-4 justify-center">
+            <li>Home</li>
+            <li>Skills</li>
+            <li>Home</li>
+            <li>Skills</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
   );
 };
 
