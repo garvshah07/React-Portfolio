d39f4c6ad6353eb8c0447252bc0638e056b5826c
garvshah07 <garv8890@gmail.com>
2024-04-05 14:00:13 +0530

    push

--- react-Portfolio/src/App.jsx
+++ react-Portfolio/src/App.jsx
@@ -1,31 +1,26 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Hero from "./components/HeroSection/Hero";
 
-import Layout from "./components/Layout";
-import About from "./components/AboutSection/About";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Hero />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Hero />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ]);
 
 function App() {
   return (
     <main className="m-auto max-w-7xl  ">
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
     </main>
   );
 }
--- react-Portfolio/src/main.jsx
+++ react-Portfolio/src/main.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Hero from "./components/HeroSection/Hero";
+import Layout from "./components/Layout";
+import About from "./components/AboutSection/About";
+
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Hero />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
