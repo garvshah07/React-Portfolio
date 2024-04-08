ff32256710c7c4b3e372a12db181b739b659ca21
garvshah07 <garv8890@gmail.com>
2024-04-08 14:20:33 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import Hero from "./components/HeroSection/Hero";
+import About from "./components/AboutSection/About";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Hero />,
-//       },
-//       {
-//         path: "about",
-//         element: <About />,
-//       },
-//     ],
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Hero />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <main className="m-auto max-w-7xl  ">
-      {/* <RouterProvider router={router} /> */}
+      <RouterProvider router={router} />
     </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Hero from "./components/HeroSection/Hero";
-import Layout from "./components/Layout";
-import About from "./components/AboutSection/About";
-
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Hero />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
