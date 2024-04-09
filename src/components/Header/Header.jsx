16e7297341978c8faafdcf5afbcbc4e0c9ce0d12
garvshah07 <garv8890@gmail.com>
2024-04-10 00:03:20 +0530

    cummit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
-import { motion, AnimatePresence } from "framer-motion";
 
 const Header = () => {
-  const variants = {
-    open: { opacity: 1, y: "0% " },
-    closed: { opacity: 1, y: 1 },
-  };
   const menuList = [
     {
       linkName: "Home",
@@ -37,64 +32,49 @@ const Header = () => {
   const logo = "GS";
 
   return (
-    <motion.div
-      // initial={{ opacity: 0, scale: 0.5 }}
-      // animate={{ opacity: 1, scale: 1 }}
-      // transition={{ duration: 0.5 }}
-
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      key="modal"
-    >
-      <header className="flex justify-center w-full">
-        <motion.nav
-          animate={isOpen ? "open" : "closed"}
-          variants={variants}
-          className="  flex justify-between h-20 cursor-pointer  gap-5  pl-10 pr-10 m-3 shadow-1 w-full  flex-wrap bg-gray-400 rounded-full backdrop-filter backdrop-blur-lg bg-opacity-10  border border-gray-700 relative"
+    <header className="flex justify-center w-full">
+      <nav className="  flex justify-between h-20 cursor-pointer  gap-5  pl-10 pr-10 m-3 shadow-1 w-full  flex-wrap bg-gray-400 rounded-full backdrop-filter backdrop-blur-lg bg-opacity-10  border border-gray-700 relative">
+        <div className="flex justify-center items-center text-center ">
+          <span className="text-lg">{logo}</span>
+        </div>
+        <div
+          className={`${
+            !isOpen
+              ? "hidden"
+              : "flex flex-col backdrop-filter bg-gray-900 backdrop-blur-lg  border border-gray-700 "
+          } md:flex md:flex-row justify-center items-center text-center gap-4 text-lg absolute right-0 top-24 w-full md:relative md:top-0  rounded-3xl md:w-auto `}
         >
-          <div className="flex justify-center items-center text-center ">
-            <span className="text-lg">{logo}</span>
-          </div>
-          <div
-            className={`${
-              !isOpen
-                ? "hidden"
-                : "flex flex-col backdrop-filter bg-gray-900 backdrop-blur-lg  border border-gray-700 "
-            } md:flex md:flex-row justify-center items-center text-center gap-4 text-lg absolute right-0 top-24 w-full md:relative md:top-0  rounded-3xl md:w-auto `}
-          >
-            {menuList.map((list, index) => {
-              return (
-                <Link
-                  key={index}
-                  onClick={linkHandler}
-                  className="p-4"
-                  to={list.url}
-                >
-                  {list.linkName}
-                </Link>
-              );
-            })}
-          </div>
+          {menuList.map((list, index) => {
+            return (
+              <Link
+                key={index}
+                onClick={linkHandler}
+                className="p-4"
+                to={list.url}
+              >
+                {list.linkName}
+              </Link>
+            );
+          })}
+        </div>
 
-          <div className="flex justify-center items-center m-3 gap-4  md:hidden">
-            {!isOpen ? (
-              <MenuIcon
-                onClick={() => {
-                  setIsOpen(true);
-                }}
-              ></MenuIcon>
-            ) : (
-              <CloseIcon
-                onClick={() => {
-                  setIsOpen(false);
-                }}
-              />
-            )}
-          </div>
-        </motion.nav>
-      </header>
-    </motion.div>
+        <div className="flex justify-center items-center m-3 gap-4  md:hidden">
+          {!isOpen ? (
+            <MenuIcon
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            ></MenuIcon>
+          ) : (
+            <CloseIcon
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            />
+          )}
+        </div>
+      </nav>
+    </header>
   );
 };
 
