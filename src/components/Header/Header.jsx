1943567fcd1d75622fe0b232dc2781a691f43dcf
garvshah07 <garv8890@gmail.com>
2024-04-09 23:17:04 +0530

    animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,13 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import { motion } from "framer-motion";
 
 const Header = () => {
+  const variants = {
+    open: { opacity: 1, y: "10% " },
+    closed: { opacity: 1, y: 1},
+  };
   const menuList = [
     {
       linkName: "Home",
@@ -32,49 +37,59 @@ const Header = () => {
   const logo = "GS";
 
   return (
-    <header className="flex justify-center w-full">
-      <nav className="  flex justify-between h-20 cursor-pointer  gap-5  pl-10 pr-10 m-3 shadow-1 w-full  flex-wrap bg-gray-400 rounded-full backdrop-filter backdrop-blur-lg bg-opacity-10  border border-gray-700 relative">
-        <div className="flex justify-center items-center text-center ">
-          <span className="text-lg">{logo}</span>
-        </div>
-        <div
-          className={`${
-            !isOpen
-              ? "hidden"
-              : "flex flex-col backdrop-filter bg-gray-900 backdrop-blur-lg  border border-gray-700 "
-          } md:flex md:flex-row justify-center items-center text-center gap-4 text-lg absolute right-0 top-24 w-full md:relative md:top-0  rounded-3xl md:w-auto `}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <header className="flex justify-center w-full">
+        <motion.nav
+          animate={isOpen ? "open" : "closed"}
+          variants={variants}
+          className="  flex justify-between h-20 cursor-pointer  gap-5  pl-10 pr-10 m-3 shadow-1 w-full  flex-wrap bg-gray-400 rounded-full backdrop-filter backdrop-blur-lg bg-opacity-10  border border-gray-700 relative"
         >
-          {menuList.map((list, index) => {
-            return (
-              <Link
-                key={index}
-                onClick={linkHandler}
-                className="p-4"
-                to={list.url}
-              >
-                {list.linkName}
-              </Link>
-            );
-          })}
-        </div>
+          <div className="flex justify-center items-center text-center ">
+            <span className="text-lg">{logo}</span>
+          </div>
+          <div
+            className={`${
+              !isOpen
+                ? "hidden"
+                : "flex flex-col backdrop-filter bg-gray-900 backdrop-blur-lg  border border-gray-700 "
+            } md:flex md:flex-row justify-center items-center text-center gap-4 text-lg absolute right-0 top-24 w-full md:relative md:top-0  rounded-3xl md:w-auto `}
+          >
+            {menuList.map((list, index) => {
+              return (
+                <Link
+                  key={index}
+                  onClick={linkHandler}
+                  className="p-4"
+                  to={list.url}
+                >
+                  {list.linkName}
+                </Link>
+              );
+            })}
+          </div>
 
-        <div className="flex justify-center items-center m-3 gap-4  md:hidden">
-          {!isOpen ? (
-            <MenuIcon
-              onClick={() => {
-                setIsOpen(true);
-              }}
-            ></MenuIcon>
-          ) : (
-            <CloseIcon
-              onClick={() => {
-                setIsOpen(false);
-              }}
-            />
-          )}
-        </div>
-      </nav>
-    </header>
+          <div className="flex justify-center items-center m-3 gap-4  md:hidden">
+            {!isOpen ? (
+              <MenuIcon
+                onClick={() => {
+                  setIsOpen(true);
+                }}
+              ></MenuIcon>
+            ) : (
+              <CloseIcon
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+              />
+            )}
+          </div>
+        </motion.nav>
+      </header>
+    </motion.div>
   );
 };
 
