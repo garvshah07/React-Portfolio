b73362eedf1db04ffef59ee0e549bb05a15dcc10
garvshah07 <garv8890@gmail.com>
2024-04-30 18:03:00 +0530

    finle changes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className=" flex justify-center items-center h-52 w-full">
+      <div >© 2024 ❤️ GARV SHAH</div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer/Footer";
 
 const Layout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
